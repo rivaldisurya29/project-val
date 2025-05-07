3c658eb60df8eab12e0340639ee92820f79f6456
Rivaldi Surya <29rivaldisuryasaputra@gmail.com>
2025-05-07 19:43:32 +0700

    Initial commit from Spring Initializr

---
+++ src/main/java/com/project_val/testval/TestvalApplication.java
@@ -0,0 +1,13 @@
+package com.project_val.testval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_val/testval/TestvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_val.testval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
